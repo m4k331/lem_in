15eb2214df99d3d3b79b3c73718dc45cea596e59
Ahugh <ahugh@student.21-school.ru>
2019-12-07 19:19:06 +0300

    update print short version 2

--- src/print_short.c
+++ src/print_short.c
@@ -6,13 +6,11 @@
 /*   By: ahugh <ahugh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/06 20:45:39 by ahugh             #+#    #+#             */
-/*   Updated: 2019/12/07 17:46:53 by ahugh            ###   ########.fr       */
+/*   Updated: 2019/12/07 19:15:01 by ahugh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
-
-
 
 static int8_t		preparing_printing(t_flow *flow, \
 										t_vector *colors, \
@@ -67,6 +65,24 @@ static inline void	push_ants_one_wave(t_flow *flow, \
 	}
 }
 
+static inline void	insert_step(char **shift, t_vector *colors, t_room *room)
+{
+	if (colors)
+	{
+		ft_memcpy(*shift, room->color, COLOR_LN);
+		(*shift) += COLOR_LN;
+	}
+	**shift = L;
+	(*shift)++;
+	(*shift) += insert_number_inline(*shift, room->ant);
+	**shift = SEP;
+	(*shift)++;
+	ft_memcpy(*shift, room->name->con, room->name->len);
+	*shift += room->name->len;
+	**shift = SPACE;
+	(*shift)++;
+}
+
 static inline void	insert_path_inline(t_path *path, \
 										t_vector *colors, \
 										char **line, \
@@ -74,29 +90,24 @@ static inline void	insert_path_inline(t_path *path, \
 {
 	t_room			**room;
 	char			*shift;
+	int8_t			skipping;
 
+	skipping = TRUE;
 	shift = *line;
-	path->rooms->iter = -1;
+	path->rooms->iter = 0;
 	room = ft_vnext_con(path->rooms);
 	while (room)
 	{
 		if ((*room)->ant > 0)
 		{
-			if (colors)
-			{
-				ft_memcpy(shift, (*room)->color, COLOR_LN);
-				shift += COLOR_LN;
-			}
-			*shift++ = L;
-			shift += insert_number_inline(shift, (*room)->ant);
-			*shift++ = SEP;
-			ft_memcpy(shift, (*room)->name->con, (*room)->name->len);
-			shift += (*room)->name->len;
-			*shift++ = SPACE;
+			skipping = FALSE;
+			insert_step(&shift, colors, *room);
 		}
+		else if (skipping == FALSE)
+			break ;
 		room = ft_vnext_con(path->rooms);
 	}
-	*len = shift - *line;
+	*len += shift - *line;
 	*line = shift;
 }
 
@@ -114,6 +125,7 @@ static inline void	print_position_ants(int fd, \
 	while (path)
 	{
 		insert_path_inline(*path, colors, &line, &len);
+		char *f = line - len;
 		path = ft_vnext_con(flow->paths);
 	}
 	if (len > 0)
@@ -126,8 +138,9 @@ static inline void	print_position_ants(int fd, \
 			len += DISCOLOR_LN;
 		}
 		*line = NL;
+		line -= len - 1;
 	}
-	write(fd, line - len + 1, len);
+	write(fd, line, len);
 }
 
 int8_t				print_short(int fd, t_vector *colors, t_flows *flows)
